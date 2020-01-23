efada439159248d9d6c972da22f98a7a00fe76de
Fabian <hello@fabiancook.dev>
2020-01-24 00:01:29 +1300

    Simplify node fs interface

--- src/store/fs-store/node/fs.ts
+++ src/store/fs-store/node/fs.ts
@@ -6,29 +6,16 @@ export async function getNodeFSStoreInterface(): Promise<FSInterface<Buffer | st
   const path = await import("path");
   return new NodeFSInterface({
     fs: {
-      readdir(name: string): Promise<string[]> | AsyncIterable<string> {
-        return fs.promises.readdir(name);
-      },
-      readFile(name: string): Promise<Buffer | string> {
-        return fs.promises.readFile(name);
-      },
-      writeFile(name: string, data: Buffer | string): Promise<void> {
-        return fs.promises.writeFile(name, data);
-      },
-      unlink(name: string): Promise<void> {
-        return fs.promises.unlink(name);
-      },
-      rmdir(name: string): Promise<void> {
-        return fs.promises.rmdir(name);
-      },
-      stat(name: string): Promise<{ isFile(): boolean; isDirectory(): boolean }> {
-        return fs.promises.stat(name);
-      }
+      readdir: fs.promises.readdir,
+      readFile: fs.promises.readFile,
+      writeFile: fs.promises.writeFile,
+      unlink: fs.promises.unlink,
+      rmdir: fs.promises.rmdir,
+      stat: fs.promises.stat
     },
     path: {
-      join(from: string, to: string): string {
-        return path.join(from, to);
-      }
-    }
+      join: path.join
+    },
+    fsExtras: undefined
   });
 }
